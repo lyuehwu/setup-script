ea457e47fe8fe43e2976a745a1706b7b2eae465d
lyuehwu <lyueh.wu@gmail.com>
2021-05-06 18:46:57 +0800

    update for vimdiff

--- setup.sh
+++ setup.sh
@@ -49,6 +49,11 @@ set expandtab   " 用 space 代替 tab
 "colorscheme desertink
 autocmd BufEnter *.txt set syntax=c
 autocmd BufEnter *.log set syntax=c
+
+highlight DiffAdd    cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
+highlight DiffDelete cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
+highlight DiffChange cterm=bold ctermfg=10 ctermbg=17 gui=none guifg=bg guibg=Red
+highlight DiffText   cterm=bold ctermfg=10 ctermbg=88 gui=none guifg=bg guibg=Red
 EOF
 
 # ~/.vimrc
